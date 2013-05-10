8aad9ffab26b5494788a27efc252eb247df7d226
mbryzhko <maxim1981@bigmir.net>
2013-05-10 23:45:09 +0300

    givenWrongTypeOfSessionWhenCreateSessionThenExceptionIsThrown

--- ServiceCommunications/src/main/java/com/makethings/communication/rpc/DefaultServiceManager.java
+++ ServiceCommunications/src/main/java/com/makethings/communication/rpc/DefaultServiceManager.java
@@ -1,22 +1,34 @@
 package com.makethings.communication.rpc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.makethings.communication.session.ApplicationSession;
 import com.makethings.communication.session.ApplicationSessionService;
 import com.makethings.communication.session.CreateSessionException;
+import com.makethings.communication.session.InMemoryApplicationSessionService;
 import com.makethings.communication.session.SessionNotFoundException;
 import com.makethings.communication.session.service.ServiceSession;
 import com.makethings.communication.session.service.ServiceSessionDefinition;
 
 public class DefaultServiceManager implements ServiceManager {
 
+    private final static Logger LOG = LoggerFactory.getLogger(DefaultServiceManager.class);
+
     private ApplicationSessionService sessionService;
 
     @Override
     public ServiceSession openServiceSession(ServiceSessionDefinition sessionDefinition) throws CreateSessionException {
+        
+        LOG.info("Creating service session for: {}", sessionDefinition.getServiceName());
+        
         ApplicationSession appSession = sessionService.createNewSession(sessionDefinition);
         ServiceSession serviceSession = null;
         if (appSession instanceof ServiceSession) {
             serviceSession = (ServiceSession) appSession;
+        }
+        else {
+            throw new CreateSessionException("Created session has worong type: " + appSession.getClass());
         }
         return serviceSession;
     }
@@ -46,7 +58,5 @@ public class DefaultServiceManager implements ServiceManager {
     public void setSessionService(ApplicationSessionService sessionService) {
         this.sessionService = sessionService;
     }
-    
-    
 
 }
--- ServiceCommunications/src/test/java/com/makethings/communication/rpc/DefaultServiceManagerTest.java
+++ ServiceCommunications/src/test/java/com/makethings/communication/rpc/DefaultServiceManagerTest.java
@@ -2,19 +2,24 @@ package com.makethings.communication.rpc;
 
 import static org.junit.Assert.assertNotNull;
 import static org.mockito.Matchers.same;
+import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.when;
 
 import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import com.makethings.communication.session.ApplicationSessionService;
+import com.makethings.communication.session.CreateSessionException;
 import com.makethings.communication.session.service.ServiceSession;
 import com.makethings.communication.session.service.ServiceSessionDefinition;
+import com.makethings.communication.session.user.UserSession;
 
 @RunWith(MockitoJUnitRunner.class)
 public class DefaultServiceManagerTest {
@@ -30,18 +35,19 @@ public class DefaultServiceManagerTest {
     @Mock
     private ServiceSession expectedOpenedSession;
 
+    @Mock
+    private UserSession userSession;
+
     private ServiceSession openedSession;
+    
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
 
     @Before
     public void setUp() {
         serviceManager = new DefaultServiceManager();
         serviceManager.setSessionService(applicationSessionService);
-
-        giveWeHaveSessionService();
-    }
-
-    private void giveWeHaveSessionService() {
-        when(applicationSessionService.createNewSession(same(serviceSessionDefinition))).thenReturn(expectedOpenedSession);
+        reset(applicationSessionService);
     }
 
     @Test
@@ -53,11 +59,30 @@ public class DefaultServiceManagerTest {
         thenServiceSessionIsCreated();
     }
 
+    @Test
+    public void givenWrongTypeOfSessionWhenCreateSessionThenExceptionIsThrown() {
+        expectExceptionWhenWrongSessionIsCreated();
+        
+        givenSessionDefinition();
+
+        whenWrongSessionTypeCreated();
+    }
+
+    private void expectExceptionWhenWrongSessionIsCreated() {
+        exception.expect(CreateSessionException.class);
+    }
+
+    private void whenWrongSessionTypeCreated() {
+        when(applicationSessionService.createNewSession(same(serviceSessionDefinition))).thenReturn(userSession);
+        openedSession = serviceManager.openServiceSession(serviceSessionDefinition);
+    }
+
     private void thenServiceSessionIsCreated() {
         Assert.assertThat(openedSession, CoreMatchers.sameInstance(expectedOpenedSession));
     }
 
     private void whenOpenSession() {
+        when(applicationSessionService.createNewSession(same(serviceSessionDefinition))).thenReturn(expectedOpenedSession);
         openedSession = serviceManager.openServiceSession(serviceSessionDefinition);
     }
 
